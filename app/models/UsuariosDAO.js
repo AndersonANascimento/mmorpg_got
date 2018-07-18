dbf7ebe537479b35dc93f80c94518cd250728229
AndersonANascimento <anderson.a.nascimento@gmail.com>
2018-07-18 15:09:33 -0400

    Otimizando códigos Final.

--- app/models/UsuariosDAO.js
+++ app/models/UsuariosDAO.js
@@ -3,12 +3,14 @@
 class UsuariosDAO {
     constructor (connection) {
         this._connection = connection();
+        this._crypto = require('crypto').createHash('md5');
         //console.log(this._connection);
     }
 
     inserirUsuario (usuario) {
         this._connection.open((err, db) => {
             db.collection("usuarios", (err, usuarios) => {
+                usuario.senha = this._crypto.update(usuario.senha).digest('hex');
                 usuarios.insert(usuario);
 
                 db.close();
@@ -19,6 +21,7 @@ class UsuariosDAO {
     login (usuario, req, res) {
         this._connection.open((err, db) => {
             db.collection("usuarios", (err, usuarios) => {
+				usuario.senha = this._crypto.update(usuario.senha).digest('hex');
                 // collection.find({usuario: {$eq: usuario.usuario}, senha: {$eq: usuario.senha}}).toArray((err, result) => {
 				usuarios.find(usuario).toArray((err, user) => {
                     if (user[0] !== undefined) {
